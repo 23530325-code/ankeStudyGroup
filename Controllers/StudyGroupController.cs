69a00cbbd539cb61f31dccabd6c7be8552851d9a
23530325-code <140383656+23530325-code@users.noreply.github.com>
2025-03-03 23:09:33 +0200

    Add files via upload

---
+++ Controllers/StudyGroupController.cs
@@ -0,0 +1,28 @@
+﻿using ankeStudyGroup.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ankeStudyGroup.Controllers
+{
+    public class StudyGroupController : Controller
+    {
+        // GET: StudyGroup
+        public ActionResult Index()
+        {
+            List<StudyGroupMember> members = new List<StudyGroupMember>
+            {
+                new StudyGroupMember { StudentNumber = "u23530325", Name = "Anke", Surname = "Vosloo", Email = "u23530325@tuks.co.za" },
+                new StudyGroupMember { StudentNumber = "u22566521", Name = "Xilia", Surname = "Mononyane", Email = "u22566521@tuks.co.za" },
+                new StudyGroupMember { StudentNumber = "u25671470", Name = "Anoeska", Surname = "Hood", Email = "u25671470@tuks.co.za" },
+                new StudyGroupMember { StudentNumber = "u22738241", Name = "Dirco", Surname = "Engelbrecht", Email = "u22738241@tuks.co.za" },
+                new StudyGroupMember { StudentNumber = "u22558307", Name = "Eliya", Surname = "Mugabe", Email = "u22558307@tuks.co.za" },
+                new StudyGroupMember { StudentNumber = "u24987230", Name = "Lesego", Surname = "Muleka", Email = "u24987230@tuks.co.za" }
+            };
+
+            return View(members);
+        }
+    }
+}
---
+++ Models/StudyGroupMember.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ankeStudyGroup.Models
+{
+    public class StudyGroupMember
+    {
+        [Key]
+        [Display(Name = "Student Number")]
+        public string StudentNumber { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Surname")]
+        public string Surname { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email Address")]
+        public string Email { get; set; }
+    }
+}
